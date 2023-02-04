7462b3952e6ffb0b22709241446f9364a4be5e16
Krystian_Pilch <krystian@solutions4delivery.com>
2023-02-04 17:33:54 +0100

    On branch master Changes to be committed:

--- Copy/JavaScript/Podstawy/remeber_javascr.js
+++
@@ -1,8 +0,0 @@
-// calllback = funkcja, którą przekazujemy jako argument do innej funkcji
-// jeżeli chcemy sprawdzić czy nasza tablica nie zawiera jakiegoś elementu "console.log(numbers.indexOf(type searching value))"";
-// Parametr podajemy podczas tworzenia funkcji -------- Argument podajemy podczas wywoływania funkcji (jest jedno i to samo natomiast inaczej określane w zależoności od czynności(tworzenie, wywoływanie)
-// Return zwraca nam nową wartość, na której możemy później pracować, podbnie jak metoda "map"
-// Z zakresu lokalnego mamy dostęp do zakresu globalnego. Z zakresu globalnego nie mamy dostępu do zakresu lokalnego
-//aaEventListener - jeżeli chcemy nadać listennera na kilku obiektach w tablicy, najpierw musimy wykonać pętle forEach 
-// setTimeout(test, 5000) - wykonuje się po np po 5 sekundach
-// setInterval(test, 1000); - wykonuje się co np sekunde
--- cypress/integration/Cypress/click.js
+++
@@ -1,9 +0,0 @@
-/// <reference types="cypress"/>
-
-describe("E2e - Akcja kliknięcia", () => {
-	it("Kliknięci", () => {
-		cy.visit("/")
-		cy.contains('[title="Contact Us"]', "Contact us").click()
-		cy.get('[title="Return to Home"]').click()
-	})
-})
